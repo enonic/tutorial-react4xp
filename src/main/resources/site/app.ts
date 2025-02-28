df6f72b8a79283e3cf178a61e1aa6046f49f3a22
Mathias Skjulestad <msk@enonic.com>
2025-02-28 10:47:59 +0100

    Handle shortcuts #422

--- src/main/resources/site/app.ts
+++ src/main/resources/site/app.ts
@@ -7,42 +7,42 @@ import {assetUrl} from '/lib/enonic/asset';
 import {type Content, get as getContentByKey} from '/lib/xp/content';
 
 export function get(request: Request): Response {
-	const url = assetUrl({path: 'images/Icon-XP.svg'});
-	let content = getContent();
-	if (content.type == "base:shortcut") {
-		const targetId: string = content.data.target as string
-		if (targetId) {
-			content = getContentByKey<Content>({key: targetId});
-			if (content) {
-				return {
-					status: 302,
-				redirect: pageUrl({path: content._path})
-			};
-		}
-	}
-		return {
-			status: 404
-		}
-	}
+    const url = assetUrl({path: 'images/Icon-XP.svg'});
+    let content = getContent();
+    if (content.type == "base:shortcut") {
+        const targetId: string = content.data.target as string
+        if (targetId) {
+            content = getContentByKey<Content>({key: targetId});
+            if (content) {
+                return {
+                    status: 302,
+                    redirect: pageUrl({path: content._path})
+                };
+            }
+        }
+        return {
+            status: 404
+        }
+    }
 
-	const {
-		component,
-		response
-	} = dataFetcher.process({
-		content, // Since it's already gotten, pass it along, so DataFetcher doesn't have to get it again.
-		request,
-	});
+    const {
+        component,
+        response
+    } = dataFetcher.process({
+        content, // Since it's already gotten, pass it along, so DataFetcher doesn't have to get it again.
+        request,
+    });
 
-	//log.info(JSON.stringify(request, null, 2))
-	if (response) {
-		return response; // This also handles the special case when ContentStudio needs 418.
-	}
-	const props: AppProps = {
-		component,
-		url
-	}
-	const react4xpId = `react4xp_${content._id}`;
-	const htmlBody = `<!DOCTYPE html>
+    //log.info(JSON.stringify(request, null, 2))
+    if (response) {
+        return response; // This also handles the special case when ContentStudio needs 418.
+    }
+    const props: AppProps = {
+        component,
+        url
+    }
+    const react4xpId = `react4xp_${content._id}`;
+    const htmlBody = `<!DOCTYPE html>
 	<html lang="en">
 		<head>
 			<meta charset="UTF-8">
@@ -53,15 +53,15 @@ export function get(request: Request): Response {
 		</body>
 	</html>`;
 
-	const output = render(
-		'App',
-		props,
-		request,
-		{
-			body: htmlBody,
-			id: react4xpId,
-		}
-	);
+    const output = render(
+        'App',
+        props,
+        request,
+        {
+            body: htmlBody,
+            id: react4xpId,
+        }
+    );
 
-	return output;
+    return output;
 }
