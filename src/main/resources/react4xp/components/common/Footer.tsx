4669aab14baecd100391668471b72fc1753d7897
Mathias Skjulestad <msk@enonic.com>
2025-03-22 17:46:17 +0100

    Accessibility #489

--- src/main/resources/react4xp/components/common/Footer.tsx
+++ src/main/resources/react4xp/components/common/Footer.tsx
@@ -10,10 +10,12 @@ export interface FooterProps {
 const Footer = ({logoUrl}: FooterProps) => (
     <footer className={styles.footer}>
         <div className={styles.footerContainer}>
+            <p>
             {`© ${new Date().getFullYear()}, Built with `}
             <a href="https://reactjs.org">React</a>
             {` and Powered by `}
             <a href="https://enonic.com">Enonic XP</a>
+            </p>
         </div>
         {/* Render logo if available */}
         {logoUrl && (
