a8a305531cf453f81c84b7950ce9f054858fb4e0
Mathias Skjulestad <msk@enonic.com>
2025-04-27 20:12:12 +0200

    repolished code #540

--- src/main/resources/react4xp/components/content/ArticleProcessor.ts
+++ src/main/resources/react4xp/components/content/ArticleProcessor.ts
@@ -8,7 +8,6 @@ import type {Content} from '@enonic-types/lib-content';
 import type {ComponentProcessorFunction} from '@enonic-types/lib-react4xp/DataFetcher';
 
 export const articleProcessor: ComponentProcessorFunction<PageDescriptor> = (params) => {
-    const parentSegment = parentPath(params.request.path);
     const {content} = params;
     const {data} = content;
 
--- src/main/resources/react4xp/components/content/Playlist.tsx
+++ src/main/resources/react4xp/components/content/Playlist.tsx
@@ -17,75 +17,75 @@ export const Playlist = (props: any) => {
     const {parent, displayName, description, movies, footerUrl} = props.playlist;
     return (
         <>
-        <div className={styles.playlist}>
+            <div className={styles.playlist}>
 
+                <div className={"back"}>
+                    <a href={parent}>
+                        <p>Back</p>
+                    </a>
+                </div>
 
-            <div className={"back"}>
-                <a href={parent}>
-                    <p>Back</p>
-                </a>
-            </div>
-            <style>
-                {`
+                <style>
+                    {`
                 body {
                     width: 100vw;
                     max-width: 100vw; /* Ensuring body does not exceed the width of the viewport */
                 }
                 `}
-            </style>
+                </style>
 
 
-            <div className={styles.titleContainer}>
-                <h1 className={styles.title}>{displayName}</h1>
-            <p className={styles.description}>{description}</p>
+                <div className={styles.titleContainer}>
+                    <h1 className={styles.title}>{displayName}</h1>
+                    <p className={styles.description}>{description}</p>
+                </div>
+                {movies && movies.length > 0 ? (
+                    <ul ref={playlistRef} className={styles.flowX}>
+                        {movies.map((movie: any, index: number) => (
+                            <li className={styles.movieItem} key={movie._id || index}>
+                                <a className={styles.movie} href={movie.movieUrl}>
+                                    <h2>{movie.title}</h2>
+                                    <p className={styles.subTitle}>{movie.subtitle}</p>
+                                    {movie.photo ? (
+                                        <div className={styles.photoBanner}>
+                                            <img
+                                                src={movie.photo.imageUrl}
+                                                alt={movie.photo.title}
+                                                title={movie.photo.title}
+                                                height={740}
+                                                width={500}
+                                                loading="eager"
+                                            />
+                                        </div>
+                                    ) : (
+                                         <p className={styles.noPhoto}> No image available</p>
+                                     )}
+                                    <p className={styles.movieDescription}>{movie.description}</p>
+                                    {movie.restPhotos && movie.restPhotos.length > 0 && (
+                                        <div className={styles.photoContainer}>
+                                            <div className={styles.additionalPhotos}>
+                                                {movie.restPhotos.map((photo: any, photoIndex: number) => (
+                                                    <img
+                                                        key={photoIndex}
+                                                        src={photo.imageUrl}
+                                                        alt={photo.title}
+                                                        title={photo.title}
+                                                        height={150}
+                                                        width={238}
+                                                        loading="lazy"
+                                                    />
+                                                ))}
+                                            </div>
+                                        </div>
+                                    )}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                ) : (
+                     <p className={styles.noMovies}>No movies in this playlist.</p>
+                 )}
             </div>
-            {movies && movies.length > 0 ? (
-                <ul ref={playlistRef} className={styles.flowX}>
-                    {movies.map((movie: any, index: number) => (
-                        <li className={styles.movieItem} key={movie._id || index}>
-                            <a className={styles.movie} href={movie.movieUrl}>
-                                <h2>{movie.title}</h2>
-                            <p className={styles.subTitle}>{movie.subtitle}</p>
-                            {movie.photo ? (
-                                <div className={styles.photoBanner}>
-                                        <img
-                                            src={movie.photo.imageUrl}
-                                            alt={movie.photo.title}
-                                            title={movie.photo.title}
-                                            height={740}
-                                            width={500}
-                                            loading="eager"
-                                        />
-                                </div>
-                            ) : (
-                                 <p className={styles.noPhoto}> No image available</p>
-                            )}
-                            <p className={styles.movieDescription}>{movie.description}</p>
-                            {movie.restPhotos && movie.restPhotos.length > 0 && (
-                                <div className={styles.photoContainer}>
-                                <div className={styles.additionalPhotos}>
-                                    {movie.restPhotos.map((photo: any, photoIndex: number) => (
-                                        <img
-                                            key={photoIndex}
-                                            src={photo.imageUrl}
-                                            alt={photo.title}
-                                            title={photo.title}
-                                            height={150}
-                                            width={238}
-                                            loading="lazy"
-                                        />
-                                    ))}
-                                </div>
-                                </div>
-                            )}
-                            </a>
-                        </li>
-                    ))}
-                </ul>
-            ) : (
-                 <p className={styles.noMovies}>No movies in this playlist.</p>
-            )}
-        </div>
             <div className={styles.mobileFooter}
                  style={{position: 'absolute', top: `calc(${playlistHeight}px + 300px)`, minHeight: 'fit-content'}}>
                 <Footer logoUrl={footerUrl}/>
