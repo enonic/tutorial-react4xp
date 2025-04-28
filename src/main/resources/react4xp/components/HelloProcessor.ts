827c103e430708755b9207bf65ddddda75ed53e1
Mathias Skjulestad <msk@enonic.com>
2025-04-28 10:21:01 +0200

    Add "99 bottles" to hello page to re-align with template to match end result. #548

--- src/main/resources/react4xp/components/HelloProcessor.ts
+++ src/main/resources/react4xp/components/HelloProcessor.ts
@@ -1,10 +1,12 @@
-import type {ComponentProcessorFunction} from '@enonic-types/lib-react4xp/DataFetcher';
+// File: src/main/resources/react4xp/components/HelloProcessor.ts
 import {PageDescriptor} from '@enonic-types/core';
+import type {ComponentProcessorFunction} from '@enonic-types/lib-react4xp/DataFetcher';
 
 export const helloProcessor: ComponentProcessorFunction<PageDescriptor> = (params) => {
-
-    return /*<HelloProps>*/{
-        title: `React4XP: ${params.content.displayName}`,
+    return {
+        title: `${params.content.displayName}`,
         text: 'Welcome to the React4XP starter!',
+        // Added a new property for the bottle count
+        initialCount: 99,
     };
 };
