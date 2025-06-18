339a76a84d1ae9ba8a4f2eddbc6ccfacb525f6a2
Pavel Milkevich <pmi@enonic.com>
2025-06-18 16:48:12 +0200

    Remove usages of <Part>, <Layout> components from user components #448

--- src/main/resources/react4xp/components/parts/ChildList.tsx
+++ src/main/resources/react4xp/components/parts/ChildList.tsx
@@ -1,20 +1,19 @@
-import {Part} from "@enonic/react-components";
 import React from "react";
 import styles from "./ChildList.module.css";
 
-
 export const ChildList = (props) => {
 
-    const {names, paths, componentRegistry, ...partProps} = props;
-    return (
-        <Part {...partProps}>
-            {names?.length > 0 && <ul>
-                {names.map((name, index) => (
-                    <li className={styles.listItem} key={index}>
-                        <a className={styles.listLink} href={paths[index]}><p>{name}</p></a>
-                    </li>
-                ))}
-            </ul>}
-        </Part>
-    );
+    const {names, paths} = props;
+
+    if (!names?.length) {
+        return;
+    }
+
+    return <ul>
+        {names.map((name, index) => (
+            <li className={styles.listItem} key={index}>
+                <a className={styles.listLink} href={paths[index]}><p>{name}</p></a>
+            </li>
+        ))}
+    </ul>
 };
