062c64d1a633eba390958bbb40ba42824f5bc386
Mathias Skjulestad <msk@enonic.com>
2025-07-07 14:48:35 +0200

    Handle empty data for parts #685 Add fallback for missing Heading and missing heading in hello component. Print "Display Name not set" if there is no displayName, same font size (set value in processor)

--- src/main/resources/react4xp/components/hello/HelloProcessor.ts
+++ src/main/resources/react4xp/components/hello/HelloProcessor.ts
@@ -2,8 +2,9 @@ import {PageDescriptor} from '@enonic-types/core';
 import type {ComponentProcessor} from '@enonic-types/lib-react4xp/DataFetcher';
 
 export const helloProcessor: ComponentProcessor<PageDescriptor> = (params) => {
+    const title = params.content?.displayName || 'Display name not set';
     return {
-        title: `${params.content.displayName}`,
+        title: title,
         initialCount: 0,
     };
 };
--- src/main/resources/react4xp/components/parts/HeadingProcessor.ts
+++ src/main/resources/react4xp/components/parts/HeadingProcessor.ts
@@ -3,7 +3,7 @@ import type {ComponentProcessor} from '@enonic-types/lib-react4xp/DataFetcher';
 
 export const headingProcessor: ComponentProcessor<'com.enonic.app.hmdb:heading'> = (params) => {
     const component = params.component as PartComponent;
-    const heading = component.config?.heading || params.content.displayName;
+    const heading = component.config?.heading || params.content.displayName || 'Display name not set';
 
     return {
         heading: heading,
