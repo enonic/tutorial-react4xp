b39823d55a329ca63c552322c9b7d792792dfef8
Mathias Skjulestad <msk@enonic.com>
2025-07-09 16:48:05 +0200

    Update hello #686 Remove HelloProps, update HelloComponent to work in RC3 of lib-react4xp and enonic types.

--- src/main/resources/react4xp/components/hello/HelloProcessor.ts
+++ src/main/resources/react4xp/components/hello/HelloProcessor.ts
@@ -2,7 +2,7 @@ import {PageDescriptor} from '@enonic-types/core';
 import type {ComponentProcessor} from '@enonic-types/lib-react4xp/DataFetcher';
 
 export const helloProcessor: ComponentProcessor<PageDescriptor> = (params) => {
-    const title = params.content?.displayName || 'Display name not set';
+    const title = params.content?.displayName || params.content._name || 'Display name not set';
     return {
         title: title,
         initialCount: 0,
