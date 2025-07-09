ed15380caf394b38d80edc5615055a69620ef7fe
Thomas Lund Sigdestad <tls@tlsair24.local>
2025-07-09 10:52:47 +0200

    cleanup + demo section

--- code-samples/code-snippets/layout/dataFetcher.ts
+++
@@ -1,9 +0,0 @@
-import {layoutProcessor} from './components/layouts/TwoColumnProcessor';
-
-...
-
-export const dataFetcher = new DataFetcher();
-
-...
-
-dataFetcher.addLayout('com.enonic.app.hmdb:2-column', {processor: layoutProcessor});
