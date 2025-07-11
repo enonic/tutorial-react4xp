a420f1eae5963c0245e42fb6a5fc27a563100e56
Mathias Skjulestad <msk@enonic.com>
2025-07-11 14:51:38 +0200

    Patch #689
    Update cross platform friendly style for Hello module.
    Update classname and minor style enhancement for macro fact-box.
    Enhance crossplatform friendlyness off movie component by rezising first image and editing media screen max-width.
    Edit global styles to over-ride some of the css module styles to ensure smooth design from start to finish.

--- src/main/resources/react4xp/components/macro/FactBox.tsx
+++ src/main/resources/react4xp/components/macro/FactBox.tsx
@@ -6,7 +6,7 @@ import styles from './FactBox.module.css';
 export const Factbox = ({config, children}: MacroComponentParams) => {
 
     return (
-        <ins className={styles.factbox}>
+        <ins className={styles.factBox}>
             <i>💬</i>
             <strong className={styles.header}>{config.header ? config.header as string : "Fact Box"}</strong>
             <div className={styles.bodyContent}> {children}</div>
