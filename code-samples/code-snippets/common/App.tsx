1f85af70d1bfcf256c6c1ddf58fec3ca61102caa
Mathias Skjulestad <msk@enonic.com>
2025-07-18 10:54:39 +0200

    #697
    Hello chapter and common chapter
    Fix broken links, update to relevant images and update code snippets.

--- code-samples/code-snippets/common/App.tsx
+++ code-samples/code-snippets/common/App.tsx
@@ -1,17 +1,19 @@
 import Footer from "/react4xp/components/common/Footer";
 import '../components/globalStyles.css'
-import type {AppProps} from '/types/AppProps';
+import {AppProps} from '/types/AppProps';
+import type {MetaData} from '@enonic/react-components';
 import {BaseComponent} from '@enonic/react-components';
 import * as React from 'react';
 import {componentRegistry} from '../componentRegistry';
 
-const App: React.FC<AppProps> = (props) => {
+const App: React.FC<AppProps> = ({component, data, common, meta}) => {
+    const compMeta: MetaData = meta as MetaData;
+    compMeta.componentRegistry = componentRegistry;
     return (
         <>
-            <BaseComponent componentRegistry={componentRegistry} data={props}/>
+            <BaseComponent component={component} data={data} common={common} meta={compMeta}/>
             {
-                (props.type == "page" || props.type == "contentType") &&
-                <Footer logoUrl={props.common.logoUrl as string}/>
+                (component.type == "page" || component.type == "contentType") && <Footer logoUrl={common.logoUrl as string}/>
             }
         </>
     );
--- code-samples/code-snippets/common/dataFetcher.ts
+++ code-samples/code-snippets/common/dataFetcher.ts
@@ -1,8 +1,7 @@
 import {DataFetcher} from '/lib/enonic/react4xp';
 import {commonProcessor} from '/react4xp/components/common/CommonProcessor';
-import {helloProcessor} from './components/HelloProcessor';
+import {helloProcessor} from './components/hello/HelloProcessor';
 
 export const dataFetcher = new DataFetcher();
 dataFetcher.addContentType('portal:site', {processor: helloProcessor});
 dataFetcher.addCommon({processor: commonProcessor});
-
